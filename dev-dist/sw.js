670dc3db229e9002ea4f70a8691a0294d07f2089
Dushime20 <fabdushime20@gmail.com>
2026-01-26 18:02:44 +0200

    add scalling to be 80 zoom

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lpos3ovokns"
+    "revision": "0.jjms2mhpc4k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
