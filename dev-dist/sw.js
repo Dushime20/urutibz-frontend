62d1c16fd127adf1faddb6d00f0fbd49445f9db7
Dushime20 <fabdushime20@gmail.com>
2026-01-27 18:06:32 +0200

    feat: Add Workbox service worker for PWA capabilities including precaching, routing, and caching strategies.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9ls2rj3mb84"
+    "revision": "0.u328d41bf4k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
